19ac14ee4c56164cb32025f670aa5dc97a8f227f
HaohanWang <haohanw@andrew.cmu.edu>
2016-08-12 13:52:57 -0400

    exp running

--- utility/evaluation.py
+++ utility/evaluation.py
@@ -3,6 +3,7 @@ __author__ = 'Haohan Wang'
 from sklearn.metrics import precision_recall_fscore_support as prfs
 from sklearn.metrics import precision_recall_curve as prc
 from sklearn.metrics import roc_curve
+from sklearn.metrics import accuracy_score
 import numpy as np
 
 def precision_recall(beta_true, beta_pred):
@@ -31,9 +32,35 @@ def clean(ll, k=100):
         r.append(l)
     return r
 
+def accuracy(y_true, y_pred_l):
+    from matplotlib import pyplot as plt
+    r = []
+    x = xrange(len(y_true))
+    for y_pred in y_pred_l:
+        m = np.median(y_pred)
+        assert m<1
+        y_pred[y_pred>m] = 1
+        y_pred[y_pred<m] = 0
+        a = accuracy_score(y_true, y_pred.astype(int))
+        r.append(a)
+        plt.scatter(x, y_pred)
+        plt.show()
+    plt.scatter(x, y_true)
+    plt.show()
+    return r
 
 if __name__ == '__main__':
     from dataLoader import EEGLoading, GenLoading
-    X, Y, Z, B = GenLoading(True)
-    beta_pred_list = np.loadtxt('../results/genomeResult.csv', delimiter=',')
-    precison_recall_curve(B, beta_pred_list, ['linear', 'L1', 'L2'])
+    # X, Y, Z, B = GenLoading(True)
+    # beta_pred_list = np.loadtxt('../results/genomeResult.csv', delimiter=',')
+    # precison_recall_curve(B, beta_pred_list, ['linear', 'L1', 'L2'])
+
+    X, Y, Z0, Z1 = EEGLoading()
+    y_pred_list = np.loadtxt('../results/EEGResult.csv', delimiter=',')
+    # print Y.shape
+    # print y_pred_list.shape
+    print accuracy(Y[10000:, 0], y_pred_list)
+
+
+
+
--- utility/experimentRunning.py
+++ utility/experimentRunning.py
@@ -9,7 +9,7 @@ from evaluation import precision_recall
 
 methods = ['linear', 'lasso', 'ridge']
 
-def runEEG():
+def runEEG(numintervals=100, ldeltamin=-5, ldeltamax=5):
     X, y, Z0, Z1 = EEGLoading()
     Y = y[:, 1]
     K = np.dot(Z0, Z0.T)
@@ -20,9 +20,9 @@ def runEEG():
     Xte = X[10000:, :]
     Kte = K[10000:, :10000]
 
-    w_linear, alp, l_linear = train(Xtr, Ktr, Ytr, mu=0, method='linear', selectK=False)
-    w_lasso, alp, l_lasso = train(Xtr, Ktr, Ytr, mu=0.01, method='lasso', selectK=False)
-    w_rd, alp, l_rd = train(Xtr, Ktr, Ytr, mu=0.01, method='ridge', selectK=False)
+    w_linear, alp, l_linear = train(Xtr, Ktr, Ytr, mu=0, numintervals=numintervals, ldeltamin=ldeltamin, ldeltamax=ldeltamax, method='linear', selectK=False)
+    w_lasso, alp, l_lasso = train(Xtr, Ktr, Ytr, mu=0, numintervals=numintervals, ldeltamin=ldeltamin, ldeltamax=ldeltamax, method='lasso', selectK=False)
+    w_rd, alp, l_rd = train(Xtr, Ktr, Ytr, mu=0, numintervals=numintervals, ldeltamin=ldeltamin, ldeltamax=ldeltamax, method='ridge', selectK=False)
 
     y_pred_linear = predict(Ytr, Xtr, Xte, Ktr, Kte, l_linear, w_linear)
     y_pred_lasso = predict(Ytr, Xtr, Xte, Ktr, Kte, l_lasso, w_lasso)
@@ -35,12 +35,12 @@ def runEEG():
     m = np.array(m)
     np.savetxt('EEGResult.csv', m, delimiter=',')
 
-def runGenome():
+def runGenome(numintervals=100, ldeltamin=-5, ldeltamax=5):
     X, Y, Z, B = GenLoading(True)
     K = np.dot(Z, Z.T)
-    w_linear, alp, l_linear = train(X, K, Y, mu=0, method='linear', selectK=True)
-    w_lasso, alp, l_lasso = train(X, K, Y, mu=0.01, method='lasso', selectK=True)
-    w_rd, alp, l_rd = train(X, K, Y, mu=0.01, method='ridge', selectK=True)
+    w_linear, alp, l_linear = train(X, K, Y, mu=0, numintervals=numintervals, ldeltamin=ldeltamin, ldeltamax=ldeltamax, method='linear', selectK=True)
+    w_lasso, alp, l_lasso = train(X, K, Y, mu=0, numintervals=numintervals, ldeltamin=ldeltamin, ldeltamax=ldeltamax, method='lasso', selectK=True)
+    w_rd, alp, l_rd = train(X, K, Y, mu=0, numintervals=numintervals, ldeltamin=ldeltamin, ldeltamax=ldeltamax, method='ridge', selectK=True)
     m = []
     m.append(w_linear)
     m.append(w_lasso)
@@ -50,5 +50,5 @@ def runGenome():
 
 
 if __name__ == '__main__':
-    runGenome()
-    # runEEG()
+    runGenome(1000, -10, 10)
+    runEEG(1000, -10, 10)
