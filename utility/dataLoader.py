37a2cb062604674200bacfeedf8ef40d28594c8c
yangta1995 <yangta1995@gmail.com>
2016-08-11 22:17:31 -0400

    Add loading generated data

--- utility/dataLoader.py
+++ utility/dataLoader.py
@@ -3,8 +3,8 @@ __author__ = 'Haohan Wang'
 import numpy as np
 from numpy import genfromtxt
 
-def dataLoading(filename):
-	file = genfromtxt(filename, delimiter=',')
+def EEGLoading():
+	file = genfromtxt('../Data/EEGdata.csv', delimiter=',')
 	data = np.asarray(file)
 	X = data[: , 2 :13 ]
 	Y = data[: , 13:15 ]
@@ -17,8 +17,24 @@ def dataLoading(filename):
 	for i in range(1, np.shape(G1)[0]):
 		Z1[i - 1][G1[i]] = 1
 	return X, Y, Z0, Z1
-<<<<<<< HEAD
-dataLoading('../Data/EEGdata.csv')
-=======
-# dataLoading('../Data/EEGdata.csv')
->>>>>>> origin/master
+
+def GenLoading(returnB = False):
+	Xdata = genfromtxt('../Data/smalldata/X.csv')
+	Ydata = genfromtxt('../Data/smalldata/Y.csv')
+	Gdata = genfromtxt('../Data/smalldata/G.csv')
+	Bdata = genfromtxt('../Data/smalldata/B.csv')
+	X = np.asarray(Xdata)
+	Y = np.asarray(Ydata)
+	G = np.asarray(Gdata)
+	B = np.asarray(Bdata)
+	Z = np.zeros([np.shape(X)[0], np.amax(G) + 1])
+	for i in range(1, np.shape(G)[0]):
+		Z[i - 1][G[i]] = 1
+	if returnB == False:	return X, Y, Z
+	else: return X, Y, Z, B
+
+# X, Y, Z, B = GenLoading(returnB=True)
+# print X.shape
+# print Y.shape
+# print Z.shape
+# print B.shape
