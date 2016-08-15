8f215b7bc872f8fa36c3cdcea687fa0285821b3c
HaohanWang <haohanw@andrew.cmu.edu>
2016-08-15 00:24:37 -0400

    exp running

--- LMM/lmm_lassoMulti.py
+++ LMM/lmm_lassoMulti.py
@@ -1,2 +1,116 @@
 __author__ = 'Haohan Wang'
 
+from lmm_lasso import *
+
+#https://github.com/yangta1995/LinearMixedModel
+
+
+def trainMulti(X, KList, y, mu, method='linear', numintervals=100, ldeltamin=-5, ldeltamax=5, selectK=True, SK=1000, regression=True, SList=None, UList=None):
+
+    [n_s, n_f] = X.shape
+    # assert X.shape[0] == y.shape[0], 'dimensions do not match'
+    # assert K.shape[0] == K.shape[1], 'dimensions do not match'
+    # assert K.shape[0] == X.shape[0], 'dimensions do not match'
+    if y.ndim == 1:
+        y = scipy.reshape(y, (n_s, 1))
+
+    # train null model
+    ldelta0 = train_nullmodel_multi(y, KList, numintervals, ldeltamin, ldeltamax, SList=SList, UList=UList)
+
+    # train lasso on residuals
+    SUX = X
+    SUy = y
+
+    for i in range(len(KList)):
+        U = UList[i]
+        S = SList[i]
+
+        delta0 = scipy.exp(ldelta0[i])
+        Sdi = 1. / (S + delta0)
+        Sdi_sqrt = scipy.sqrt(Sdi)
+        SUX = scipy.dot(U.T, SUX)
+        SUX = SUX * scipy.tile(Sdi_sqrt, (n_f, 1)).T
+        if regression:
+            SUy = scipy.dot(U.T, y)
+            SUy = SUy * scipy.reshape(Sdi_sqrt, (n_s, 1))
+        else:
+            SUy = y.astype(int).reshape(y.shape[0])
+
+    print SUy.shape
+
+    if method == 'linear':
+        w, clf = train_linear(SUX, SUy, mu, method, regression)
+        alpha = 0
+    else:
+        regList = []
+        for i in range(10):
+            regList.append(10 ** (i-5))
+        alpha, ss = cv_train(SUX, SUy, regList, method, selectK, K=SK, regression=regression)
+        w, clf = train_linear(SUX, SUy, alpha, method, regression)
+
+    return w, alpha, ldelta0, clf
+
+
+def train_nullmodel_multi(y, KList, numintervals=500, ldeltamin=-5, ldeltamax=5, scale=0, SList=None, UList=None):
+    """
+    train random effects model:
+    min_{delta}  1/2(n_s*log(2pi) + logdet(K) + 1/ss * y^T(K + deltaI)^{-1}y,
+
+    Input:
+    X: Snp matrix: n_s x n_f
+    y: phenotype:  n_s x 1
+    K: kinship matrix: n_s x n_s
+    mu: l1-penalty parameter
+    numintervals: number of intervals for delta linesearch
+    ldeltamin: minimal delta value (log-space)
+    ldeltamax: maximal delta value (log-space)
+    """
+    ldeltamin += scale
+    ldeltamax += scale
+
+    n_s = y.shape[0]
+
+    # rotate data
+    if SList is None or UList is None:
+        SList = []
+        UList = []
+        for i in range(len(KList)):
+            S, U = linalg.eigh(KList[i])
+            SList.append(S)
+            UList.append(U)
+
+    lg_list = []
+
+    for i in range(len(KList)):
+        U = UList[i]
+        S = SList[i]
+
+        Uy = scipy.dot(U.T, y)
+
+        # grid search
+        nllgrid = scipy.ones(numintervals + 1) * scipy.inf
+        ldeltagrid = scipy.arange(numintervals + 1) / (numintervals * 1.0) * (ldeltamax - ldeltamin) + ldeltamin
+        nllmin = scipy.inf
+        for i in scipy.arange(numintervals + 1):
+            nllgrid[i] = nLLeval(ldeltagrid[i], Uy, S)
+
+        # find minimum
+        nllmin = nllgrid.min()
+        ldeltaopt_glob = ldeltagrid[nllgrid.argmin()]
+
+        # more accurate search around the minimum of the grid search
+
+        for i in scipy.arange(numintervals - 1) + 1:
+            if (nllgrid[i] < nllgrid[i - 1] and nllgrid[i] < nllgrid[i + 1]):
+                ldeltaopt, nllopt, iter, funcalls = opt.brent(nLLeval, (Uy, S),
+                                                              (ldeltagrid[i - 1], ldeltagrid[i], ldeltagrid[i + 1]),
+                                                              full_output=True)
+                if nllopt < nllmin:
+                    nllmin = nllopt
+                    ldeltaopt_glob = ldeltaopt
+
+        lg_list.append(ldeltaopt_glob)
+
+    return lg_list
+
+
--- utility/experimentRunning.py
+++ utility/experimentRunning.py
@@ -6,6 +6,7 @@ sys.path.append('../')
 
 from dataLoader import GenLoading, EEGLoading, EEGLoading_KSU, GenLoadingKSU
 from LMM.lmm_lasso import *
+from LMM.lmm_lassoMulti import *
 from evaluation import precision_recall
 
 methods = ['linear', 'lasso', 'ridge']
@@ -19,7 +20,13 @@ def runEEG(numintervals=100, ldeltamin=-5, ldeltamax=5):
         Y = y[:, l]
         for i in range(3):
             print 'con ', i
-            K, U, S = EEGLoading_KSU(i)
+            if i == 0:
+                K = np.dot(Z0, Z0.T)
+            elif i == 1:
+                K = np.dot(Z1, Z1.T)
+            else:
+                Z2 = np.append(Z0, Z1, 1)
+                K = np.dot(Z2, Z2.T)
 
             Xtr = X[:10000, :]
             Ytr = Y[:10000]
@@ -27,13 +34,13 @@ def runEEG(numintervals=100, ldeltamin=-5, ldeltamax=5):
 
             print 'linear'
             w_linear, alp, l_linear, clf_linear = train(Xtr, K, Ytr, mu=0, numintervals=numintervals, ldeltamin=ldeltamin,
-                                                        ldeltamax=ldeltamax, method='linear', selectK=False, regression=False, S=S, U=U)
+                                                        ldeltamax=ldeltamax, method='linear', selectK=False, regression=False)
             print 'lasso'
             w_lasso, alp, l_lasso, clf_lasso = train(Xtr, K, Ytr, mu=0, numintervals=numintervals, ldeltamin=ldeltamin,
-                                                     ldeltamax=ldeltamax, method='lasso', selectK=False, regression=False, S=S, U=U)
+                                                     ldeltamax=ldeltamax, method='lasso', selectK=False, regression=False)
             print 'ridge'
             w_rd, alp, l_rd, clf_rd = train(Xtr, K, Ytr, mu=0, numintervals=numintervals, ldeltamin=ldeltamin,
-                                            ldeltamax=ldeltamax, method='ridge', selectK=False, regression=False, S=S, U=U)
+                                            ldeltamax=ldeltamax, method='ridge', selectK=False, regression=False)
 
             y_pred_linear = predict(Xte, None, l_linear, clf_linear)
             y_pred_lasso = predict(Xte, None, l_lasso, clf_lasso)
@@ -45,6 +52,33 @@ def runEEG(numintervals=100, ldeltamin=-5, ldeltamax=5):
             m.append(y_pred_rd)
             m = np.array(m)
             np.savetxt('../results/EEGResult_label_'+str(l+1)+'_con_'+str(i+1)+'.csv', m, delimiter=',')
+
+        KList = [np.dot(Z0, Z0.T), np.dot(Z1, Z1.T)]
+        Xtr = X[:10000, :]
+        Ytr = Y[:10000]
+        Xte = X[10000:, :]
+
+        print 'linear'
+        w_linear, alp, l_linear, clf_linear = trainMulti(Xtr, KList, Ytr, mu=0, numintervals=numintervals, ldeltamin=ldeltamin,
+                                                    ldeltamax=ldeltamax, method='linear', selectK=False, regression=False)
+        print 'lasso'
+        w_lasso, alp, l_lasso, clf_lasso = trainMulti(Xtr, K, Ytr, mu=0, numintervals=numintervals, ldeltamin=ldeltamin,
+                                                 ldeltamax=ldeltamax, method='lasso', selectK=False, regression=False)
+        print 'ridge'
+        w_rd, alp, l_rd, clf_rd = trainMulti(Xtr, K, Ytr, mu=0, numintervals=numintervals, ldeltamin=ldeltamin,
+                                        ldeltamax=ldeltamax, method='ridge', selectK=False, regression=False)
+
+        y_pred_linear = predict(Xte, None, l_linear, clf_linear)
+        y_pred_lasso = predict(Xte, None, l_lasso, clf_lasso)
+        y_pred_rd = predict(Xte, None, l_rd, clf_rd)
+
+        m = []
+        m.append(y_pred_linear)
+        m.append(y_pred_lasso)
+        m.append(y_pred_rd)
+        m = np.array(m)
+        np.savetxt('../results/EEGResult_label_'+str(l+1)+'_con_'+str(3)+'.csv', m, delimiter=',')
+
 
 
 def runGenome(numintervals=100, ldeltamin=-5, ldeltamax=5):
@@ -64,6 +98,24 @@ def runGenome(numintervals=100, ldeltamin=-5, ldeltamax=5):
         m.append(w_rd)
         m = np.array(m)
         np.savetxt('../results/genomeResult_con_'+str(i+1)+'.csv', m, delimiter=',')
+
+    K0, U0, S0 = GenLoadingKSU(0)
+    K1, U1, S1 = GenLoadingKSU(1)
+    KList = [K0, K1]
+    UList = [U0, U1]
+    SList = [S0, S1]
+    w_linear, alp, l_linear, clf_linear = trainMulti(X, KList, Y, mu=0, numintervals=numintervals, ldeltamin=ldeltamin,
+                                                ldeltamax=ldeltamax, method='linear', selectK=True, regression=True, SList=SList, UList=UList)
+    w_lasso, alp, l_lasso, clf_lasso = trainMulti(X, KList, Y, mu=0, numintervals=numintervals, ldeltamin=ldeltamin,
+                                             ldeltamax=ldeltamax, method='lasso', selectK=True, regression=True, SList=SList, UList=UList)
+    w_rd, alp, l_rd, clf_rd = trainMulti(X, KList, Y, mu=0, numintervals=numintervals, ldeltamin=ldeltamin, ldeltamax=ldeltamax,
+                                    method='ridge', selectK=True, regression=True, SList=SList, UList=UList)
+    m = []
+    m.append(w_linear)
+    m.append(w_lasso)
+    m.append(w_rd)
+    m = np.array(m)
+    np.savetxt('../results/genomeResult_con_'+str(3)+'.csv', m, delimiter=',')
 
 
 if __name__ == '__main__':
